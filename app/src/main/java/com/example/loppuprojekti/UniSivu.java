a5015bc098096014bffcf5157ae0fbc43c7bcc2e
nikoa-a <niko.alaaho@gmail.com>
2021-04-29 11:35:03 +0300

    ekacommit

---
+++ app/src/main/java/com/example/loppuprojekti/UniSivu.java
@@ -0,0 +1,14 @@
+package com.example.loppuprojekti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UniSivu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uni_sivu);
+    }
+}
