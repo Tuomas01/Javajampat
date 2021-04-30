cbcbe304621a8447bdd941a5fcd4152fea384270
nikoa-a <niko.alaaho@gmail.com>
2021-04-30 15:46:03 +0300

    toinecommit

--- app/src/main/java/com/example/loppuprojekti/UniSivu.java
+++ app/src/main/java/com/example/loppuprojekti/UniSivu.java
@@ -3,12 +3,22 @@ package com.example.loppuprojekti;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toolbar;
 
 public class UniSivu extends AppCompatActivity {
+
+    Toolbar topbar;
+    ImageView sleepIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uni_sivu);
+
+
+        sleepIcon = findViewById(R.id.sleepButton);
+        topbar = findViewById(R.id.toolbar);
+        sleepIcon.setBackgroundColor(0xFF2196F3);
     }
 }
