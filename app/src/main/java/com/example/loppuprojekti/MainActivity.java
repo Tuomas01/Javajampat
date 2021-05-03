79a89fd268d6a53b263e8b4eaf1268ab783ff39e
Tumbe <tumppi.alatalo@gmail.com>
2021-05-03 14:16:03 +0300

    commit

--- app/src/main/java/com/example/loppuprojekti/MainActivity.java
+++ app/src/main/java/com/example/loppuprojekti/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.loppuprojekti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView training;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        training = (ImageView) findViewById(R.id.trainingButton);
+        training.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openTraining();
+            }
+        });
+    }
+    public void openTraining() {
+        Intent intent = new Intent(this, TrainingActivity.class);
+        startActivity(intent);
     }
 }
